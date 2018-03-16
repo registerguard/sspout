d26aba4397b7610168cf01e67509abebdf9bf055
Rob Denton <robert.denton9@gmail.com>
2018-03-15 17:01:31 -0700

    Got SSP coming in

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Python project to do the following:
 
-* Loop over DT photo gallery stories (web.gal-) and grab metadata (including SSP id)
-* Go grab metadata from SSP API
-* Go grab images from SSP
-* Package each story up into folder with XML and images for NCS importing
+* [x] Loop over DT photo gallery stories (web.gal-) and grab metadata (including SSP id)
+* [x] Go grab metadata from SSP API
+* [ ] Figure out date folder structure
+* [ ] Go grab images from SSP
+* [ ] Package each story up into folder with XML and images for NCS importing
 
 ## Preliminary research
 
@@ -17,3 +18,57 @@ There are between 337 and 360 stories slotted with the gallery subcategories. I 
 * 360 is the number returned by a db query: `SELECT storyId FROM dbo.Story WHERE Story.subCategoryId in (32058824,32067956,32058769,32058823,32058773,32058730,32058735,32058827,32058745,31994432,32058749,32058736,32058195,32058817,32068003,32058784,32058756)`
 
 *Side note: When you do the JSON query, there is an error returned.*
+
+Wrote out first CSV file on 3/14. 
+
+Story data structure from DT API looks like this (clean and raw):
+
+* byline
+* category
+* cat id
+* count (total stories returned)
+* deck
+* headline
+* path (full URL)
+* publish date (YYYY-MM-DD HH:MM:SS)
+* server (???)
+* author
+* excerpt
+* sspid
+* image (DT 990 file)
+
+```
+[{'byline': 'The Register-Guard Staff', 'category': 'Sports', 'catid': '32058784', 'count': 1, 'deck': 'From Willamette High to the NCAA Tournament, half a decadeof Lexi Bando in pictures', 'headline': 'The Bando bandwagon', 'path': 'http://registerguard.com/rg/photo/36550000-321/the-bando-bandwagon.html.csp', 'published':'2018-03-14 14:30:01', 'server': '', 'author': 'The Register-Guard Staff', 'excerpt': '', 'sspid': '679', 'image': 'http://registerguard.com/csp/cms/sites/dt.common.streams.StreamServer.cls?STREAMOID=I6uQtLL7vX76Kmq_8xB8Y8$daE2N3K4ZzOUsqbU5sYuSguc0JOxw0CVWS_uWjGHFWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&amp;CONTENTTYPE=image/jpeg'}]
+```
+
+Example gallery field mapping for NCS
+```xml
+<?xml version="1.0" encoding="UTF-8" ?>
+<gallery>
+	<uniqueid></uniqueid>
+	<title><![CDATA[]]></title>
+	<date></date>
+    <category>PHOTOGALLERY</category>
+	<taxonomies></taxonomies>
+	<description>GALLERY CAPTION</description>
+	<images>
+        <image>
+            <title><![CDATA[]]></title>
+            <caption><![CDATA[]]></caption>
+            <credit><![CDATA[]]></credit>
+            <filename></filename>
+        </image>
+	</images>
+	<seo-label></seo-label>
+</gallery>
+```
+
+Data coming out of [SSP API](http://slideshow.registerguard.com/slideshowpro/api/ncs/index.php?id=404):
+
+* image (990)
+* thumb (150)
+* byline (name only)
+* description (caption)
+* id (unique ssp id)
+* filename (original filename)
+* original (original image file)
--- main.py
+++ main.py
@@ -1,24 +1,103 @@
-import requests, lxml, json, csv
+import requests, json, csv
+from lxml import etree
 from bs4 import BeautifulSoup
+from datetime import datetime
 
+def getStories(subcats,items=10):
+    # raw_input for items ???
+    payload = {'subcats': subcats, 'items': items}
+    url = 'http://registerguard.com/csp/cms/sites/rg/feeds/json.csp'
+    r = requests.get(url, params=payload)
+    json = r.json()
+    #hits = json['hits']
+    stories = json['stories']
+    return stories
+
+def getAlbum(sspalbum):
+    # raw_input for items ???
+    payload = {'id': sspalbum}
+    url = 'http://slideshow.registerguard.com/slideshowpro/api/ncs/index.php'
+    r = requests.get(url, params=payload)
+    album = r.json()
+    return album
+
+def storyCSV(stories,csvname='stories.csv'):
+    # Error checking for csv extension???
+    print("Writing {0}...".format(csvname))
+    with open(csvname,'w',newline='') as csvfile:
+        fieldnames = ['headline','url','author','pubdate']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for story in stories:
+            writer.writerow({'headline': story['headline'], 'url': story['path'], 'author': story['byline'], 'pubdate': story['published']})
+            #print(story['headline'])
+    print("{0} has been written.".format(csvname))
+
+def writeXML(stories,xmlname='out.xml'):
+    ### Write XML
+    for story in stories:
+        gallery = etree.Element('gallery')
+        # Gallery story metadata
+        uniqueid = etree.SubElement(gallery,'uniqueid')
+        uniqueid.text = story['id']
+        title = etree.SubElement(gallery,'title')
+        title.text = etree.CDATA(story['headline'])
+        date = etree.SubElement(gallery,'date')
+        # Do smart date stuff
+        dateTEMP = story['published']
+        dateTEMP = datetime.strptime(dateTEMP, '%Y-%m-%d %H:%M:%S')
+        date.text = dateTEMP.strftime('%Y-%m-%dT%H:%M:%S')
+        category = etree.SubElement(gallery,'category')
+        category.text = story['section']
+        taxonomy = etree.SubElement(gallery,'taxonomy')
+        taxonomy.text = story['catid']
+        description = etree.SubElement(gallery,'description')
+        description.text = etree.CDATA(story['excerpt'])
+        # Move into images
+        album = getAlbum(story['sspid'])
+        #print(story['sspid'])
+        images = etree.SubElement(gallery, 'images')
+        for pic in album:
+            image = etree.SubElement(images, 'image')
+            title = etree.SubElement(image,'title')
+            title.text = etree.CDATA(pic['id'])
+            # Make sure caption is clean
+            caption = etree.SubElement(image,'caption')
+            captionTEMP = pic['description']
+            captionTEMP = captionTEMP.replace('\n','').replace('\r','').replace('\t','')
+            caption.text = etree.CDATA(captionTEMP)
+            # Make sure credit is clean
+            credit = etree.SubElement(image,'credit')
+            creditTEMP = pic['byline']
+            creditTEMP = creditTEMP.replace('\n','').replace('\r','').replace('\t','')
+            credit.text = etree.CDATA(pic['byline'])
+            filename = etree.SubElement(image,'filename')
+            filename.text = etree.CDATA(pic['filename'])
+            # print(etree.tostring(images, pretty_print=True))
+        # Move into exporting to file
+        # print(etree.tostring(gallery, pretty_print=True))
+        out = etree.ElementTree(gallery)
+        out.write(xmlname, pretty_print=True, xml_declaration=True, encoding='utf-8')
+
+# DT API variables
 photoSubcats = "32058824,32067956,32058769,32058823,32058773,32058730,32058735,32058827,32058745,31994432,32058749,32058736,32058195,32058817,32068003,32058784,32058756"
 videoSubcats = "31994433,32058759,32058196,32003307,32058816,32042463,32042459,32058748,31994425,32058825,32042583,32042464,32058820,32058774,32042460,32003311,32058826,32058162"
 items = 350
-payload = {'subcats': photoSubcats, 'items': items}
-url = 'http://registerguard.com/csp/cms/sites/rg/feeds/json.csp'
-r = requests.get(url, params=payload)
 
-#print(r.status_code)
+# Get stories from DT API (subcat id list, number to return)
+stories = getStories(photoSubcats,1)
+#print(stories)
+
+# Write those stories to a CSV (stories var, name of output csv)
+#storyCSV(stories,'test.csv')
+
+# SSP API variables
+#sspalbum = 404
+#album = getAlbum(sspalbum)
+
+# Write out to XML
+writeXML(stories, 'test.xml')
 
 
-json = r.json()
-stories = json['stories']
 
-with open('photogalleries.csv','w',newline='') as csvfile:
-    fieldnames = ['headline','url','author','pubdate']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for story in stories:
-        writer.writerow({'headline': story['headline'], 'url': story['path'], 'author': story['author'], 'pubdate': story['published']})
-        #print(story['headline'])
 
