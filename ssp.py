ca185af3e4ebe61a91a1b2951732dc446edd7245
Rob Denton <robert.denton9@gmail.com>
2018-03-30 12:06:07 -0700

    Fix Gallery title, add ssp

--- ssp.py
+++ ssp.py
@@ -7,6 +7,7 @@ from scripts import *
 
 # Global vars
 cwd = os.getcwd()
+rgfiles = '/Volumes/newsweb\$/'
 photoSubcats = "32058824,32067956,32058769,32058823,32058773,32058730,32058735,32058827,32058745,31994432,32058749,32058736,32058195,32058817,32068003,32058784,32058756"
 #videoSubcats = "31994433,32058759,32058196,32003307,32058816,32042463,32042459,32058748,31994425,32058825,32042583,32042464,32058820,32058774,32042460,32003311,32058826,32058162"
 items = 999
@@ -64,8 +65,7 @@ def writeGalleryXML(stories):
                 for pic in album:
                     image = etree.SubElement(images, 'image')
                     title = etree.SubElement(image,'title')
-                    #title.text = etree.CDATA(pic['id'])
-                    title.text = ""
+                    title.text = etree.CDATA(pic['id'])
                     # Make sure caption is clean
                     caption = etree.SubElement(image,'caption')
                     captionTEMP = pic['description']
